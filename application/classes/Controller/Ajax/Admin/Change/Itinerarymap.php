6b6c327a73fc7d9ffb2d3d84bdcf5b8d36326cab
walter <lorenzetti@gis3w.it>
2014-04-22 16:04:22 +0200

    Cmbiato il sistema di recupero cchange per multiselect.

--- application/classes/Controller/Ajax/Admin/Change/Itinerarymap.php
+++ application/classes/Controller/Ajax/Admin/Change/Itinerarymap.php
@@ -4,49 +4,61 @@
 class Controller_Ajax_Admin_Change_itinerarymap extends Controller_Ajax_Auth_Strict{
     
     public function action_index() {
-        if(isset($_GET['poi_id']) AND is_numeric($_GET['poi_id']))
+        if(isset($_GET['poi_id']) AND $_GET['poi_id'] != 'null')
             $this->_get_poi();
         
-         if(isset($_GET['path_id']) AND is_numeric($_GET['path_id']))
+         if(isset($_GET['path_id']) AND $_GET['path_id'] != 'null')
             $this->_get_path();
              
     }
     
     protected function _get_poi()
     {
-        $poi = ORMGIS::factory('Poi',$_GET['poi_id']);
+        $poi_ids = preg_split('/,/',$_GET['poi_id']);
+        $pois = ORMGIS::factory('Poi')
+                ->where('id','IN',DB::expr('('. implode(',', $poi_ids).')'));
+        
         $toData = array();
         
         $toData['disabled'] = FALSE;
         $toData['value'] = array(
-            'items' => array(
-                array(
+            'items' => array(),
+        );
+        
+        foreach($pois as $poi)
+        {
+            $toData['value']['items'][] =array(
                     'id' => $poi->id,
                     'typology_id' => (int)$poi->typology_id,
                     'geoJSON' => json_decode($poi->asgeojson)
-                ),
-            ),
-        );
+                );
+        }
         
         $this->jres->data->the_geom = $toData;
     }
     
      protected function _get_path()
     {
-        $path = ORMGIS::factory('Path',$_GET['path_id']);
+        $path_ids = preg_split('/,/',$_GET['path_id']);
+        $paths = ORMGIS::factory('Path')
+                ->where('id','IN',DB::expr('('. implode(',', $path_ids).')'));
+        
         $toData = array();
         
         $toData['disabled'] = FALSE;
         $toData['value'] = array(
-            'items' => array(
-                array(
+            'items' => array(),
+        );
+        
+        foreach($paths as $path)
+        {
+            $toData['value']['items'][] =array(
                     'id' => $path->id,
                     'typology_id' => (int)$path->typology_id,
-                    'geoJSON' => json_decode($path->asgeojson),
-                    'color' => $path->color
-                ),
-            ),
-        );
+                    'color' => $path->color,
+                    'geoJSON' => json_decode($path->asgeojson)
+                );
+        }
         
         $this->jres->data->the_geom= $toData;
     }
