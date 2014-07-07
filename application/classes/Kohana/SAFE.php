c818d0c3ec0a4518e7dae2105c26d2b1da345dd8
wlorenzetti <lorenzetti@gis3w.it>
2014-07-07 09:49:20 +0200

    Commit per versione 0.1.8

--- application/classes/Kohana/SAFE.php
+++ application/classes/Kohana/SAFE.php
@@ -13,7 +13,7 @@
 class Kohana_SAFE extends Kohana_Core
 {
     
-    const VERSION = '0.1.7';
+    const VERSION = '0.1.8';
     
     /**
      * Overload del metodo per avere anche la traduzione compresa
