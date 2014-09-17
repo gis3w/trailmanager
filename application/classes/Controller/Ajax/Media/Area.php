42b4969aacc79b13512e12694003bc88952dc0be
walter <lorenzetti@gis3w.it>
2014-09-17 15:56:40 +0200

    Fixed bug on media/area rest frontend call. Take data from Path table instead Area

--- application/classes/Controller/Ajax/Media/Area.php
+++ application/classes/Controller/Ajax/Media/Area.php
@@ -5,7 +5,7 @@ class Controller_Ajax_Media_Area extends Controller_Ajax_Media_Base{
     
     protected $_pagination = FALSE;
     
-    protected $_datastruct = "Path";
+    protected $_datastruct = "Area";
     
     protected $_image_uri ="/download/imagearea/index/"; 
     protected $_image_thumb_uri ="/download/imagearea/thumbnail/"; 
