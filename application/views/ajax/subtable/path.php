43d0c727074478ad34bc0fd95b2049468af8e945
wlorenzetti <lorenzetti@gis3w.it>
2015-07-07 11:11:42 +0200

    Add subtable logic and example for poi.

--- application/views/ajax/subtable/path.php
+++ application/views/ajax/subtable/path.php
@@ -4,12 +4,15 @@ $items = $jres->data->items;
 ?>
 
 
-<table>
+<table class="table table-stripped">
+    <thead>
     <tr>
         <th><?php echo _('SE')?></th>
         <th><?php echo _('Origin description')?></th>
         <th><?php echo _('Link')?></th>
     </tr>
+    </thead>
+    <tbody>
     <?php foreach ($items as $item): ?>
         <tr>
             <td><?php echo $item['se'] ?></td>
@@ -17,4 +20,5 @@ $items = $jres->data->items;
             <td><a class="btn" href="/admin/#path/<?php echo $item['id'] ?>"><?php echo $item['id'] ?></a></td>
         </tr>
     <?php endforeach ?>
+    </tbody>
 </table>
