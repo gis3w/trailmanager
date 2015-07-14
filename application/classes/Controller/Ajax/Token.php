c5c4db1555ef7f90c55f040f87443fb005763178
wlorenzetti <lorenzetti@gis3w.it>
2015-07-14 15:51:01 +0200

    Fix auth strict on datastruct api.

--- application/classes/Controller/Ajax/Token.php
+++ application/classes/Controller/Ajax/Token.php
@@ -1,13 +1,25 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 
-class Controller_Ajax_Token extends Controller_Ajax_Auth_Strict{
-
-
+class Controller_Ajax_Token extends Controller_Ajax_Main{
+    
     public function action_index() {
-             $this->jres->data->token = md5($this->user->username.time());
+            if(Auth::instance()->logged_in())
+            {
+                $this->jres->data->token = md5(Auth::instance()->get_user()->username.time());
+            }
+            else
+            {
+                $this->jres->data->token = md5('anonimous'.time());
+            }
+            
+            // token name
+            $token_name = isset($_GET['datastruct']) ? $_GET['datastruct'] : '';
+            
+            $token_name = 'token'.$token_name;
+            
              //aggiornamento della sessione 
-             $this->session->set('token',$this->jres->data->token);
+             Session::instance()->set($token_name,$this->jres->data->token);
     }
 }
    
