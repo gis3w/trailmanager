a686abdeea25fd97f3c709b3103c83a53205c05e
wlorenzetti <lorenzetti@gis3w.it>
2015-07-14 17:47:58 +0200

    Rebuild heights profile path API for c3.js format data.

--- application/classes/Controller/Ajax/Heightsprofilepath.php
+++ application/classes/Controller/Ajax/Heightsprofilepath.php
@@ -16,7 +16,18 @@ class Controller_Ajax_Heightsprofilepath extends Controller_Ajax_Base_GET{
             throw new HTTP_Exception_500(SAFE::message('ehttp','500_path_orm'));
 
         //we get heights_profile data
-        $this->jres->data = $path->heights_profile->find_all()->as_array();
+        $heights_profile_data = $path->heights_profile->find_all()->as_array();
+        // rebuild data for C3.js
+        $chartdata = [
+            ['x'],
+            ['data-'.$path_id]
+        ];
+        foreach($heights_profile_data as $data)
+        {
+            $chartdata[0][] = (float)$data->cds2d;
+            $chartdata[1][] = (float)$data->z;
+        }
+        $this->jres->data = $chartdata;
 
     }
 }
