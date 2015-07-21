e60cf7ae0adc8416c9a890b4b61d6f8cbbe30a02
wlorenzetti <lorenzetti@gis3w.it>
2015-07-21 17:06:34 +0200

    Add path close api call.

--- modules/highliting/classes/Controller/Ajax/Pathsclose.php
+++ modules/highliting/classes/Controller/Ajax/Pathsclose.php
@@ -8,10 +8,26 @@ class Controller_Ajax_Pathsclose extends Controller_Ajax_Main{
         $lon = $this->request->param('lon');
         $lat = $this->request->param('lat');
 
+        $maxDistance = 100;
+
         if (!isset($lon) OR ! isset($lat))
             throw new HTTP_Exception_500(SAFE::message('ehttp','no_lon_or_lat_param'));
+        $paths = ORMGIS::factory('Path');
+        $geoPOINT = "ST_Transform(ST_SetSRID(ST_Point(".$lon.",".$lat."),4326),".$paths->epsg_db.")";
 
+        // build distance query
+        $paths = $paths->where(DB::expr("(ST_Distance(the_geom,".$geoPOINT."))"),'<=',$maxDistance)
+            ->find_all();
 
+        if(count($paths) >= 0)
+            $this->jres->data->items=[];
+        foreach($paths as $path)
+        {
+            $this->jres->data->items[]  = [
+                'id' => $path->pk(),
+                'title' => $path->title,
+            ];
+        }
     }
 
     }
