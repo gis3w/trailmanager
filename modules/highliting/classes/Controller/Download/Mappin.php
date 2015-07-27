d6cd972c7e4f5ca15d4c72ad0ff71dfd1f24b242
wlorenzetti <lorenzetti@gis3w.it>
2015-07-27 10:57:38 +0200

    Ccontinue develop, add mappin download route.

---
+++ modules/highliting/classes/Controller/Download/Mappin.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Download_Mappin extends Controller_Download_Image {
+    
+    public static $subpathUpload = 'mappin';
+    
+}   
