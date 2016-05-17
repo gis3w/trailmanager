4a3df0c33fd5d7957d75d91b2f5f09c02f8a05e5
wlorenzetti <lorenzetti@gis3w.it>
2016-05-17 18:38:30 +0200

    Continue develop.

--- modules/highliting/classes/Controller/Ajax/Highliting.php
+++ modules/highliting/classes/Controller/Ajax/Highliting.php
@@ -1,11 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 
-class Controller_Ajax_Data_Everytype extends Controller_Ajax_Main{
+class Controller_Ajax_Highliting extends Controller_Ajax_Main{
 
-    use Controller_Ajax_Base_Cache_GET;
+    #use Controller_Ajax_Base_Cache_GET;
 
-    protected $_base_route = 'jx/data';
+    protected $_base_route = 'jx';
 
 
     public function action_delete() {
@@ -30,24 +30,16 @@ class Controller_Ajax_Data_Everytype extends Controller_Ajax_Main{
     
     protected function _get_all_highlightins()
     {
-        $poi = json_decode(Request::factory(Route::url($this->_base_route, array('controller' => 'poi')))
-                            ->execute()
-                            ->body());
-        $path = json_decode(Request::factory(Route::url($this->_base_route, array('controller' => 'path')))
-                            ->execute()
-                            ->body());
-        $area = json_decode(Request::factory(Route::url($this->_base_route, array('controller' => 'area')))
-                            ->execute()
-                            ->body());
 
+        $highliting_poi = json_decode(Request::factory(Route::url($this->_base_route, array('controller' => 'highlitingpoi')))
+                            ->execute()
+                            ->body());
 
         $items = array(
-            'Poi' => $poi->data->items,
-            'Path' => $path->data->items,
-            'Area' => $area->data->items,
+            'Poi' => $highliting_poi->data->items,
         );
         
-        $this->jres->data->tot_items = $this->jres->data->items_per_page =  count($poi->data->items) + count($path->data->items) + count($area->data->items);
+        $this->jres->data->tot_items = $this->jres->data->items_per_page =  count($highliting_poi->data->items);
         $this->jres->data->items = $items;
        
     }
--- modules/highliting/classes/Controller/Ajax/Highlitingpoi.php
+++ modules/highliting/classes/Controller/Ajax/Highlitingpoi.php
@@ -17,6 +17,9 @@ class Controller_Ajax_Highlitingpoi extends Controller_Ajax_Admin_Base_Highlitin
         'front_image_highliting_poi' => 'Image_Highliting_Poi',
     );
 
+    protected $_image_uri ="/download/imagehighlitingpoi/index/";
+    protected $_image_thumb_uri ="/download/imagehighlitingpoi/thumbnail/";
+
     public function action_update() {
         throw new HTTP_Exception_500(SAFE::message('ehttp','invalid_operation'));
     }
@@ -24,10 +27,11 @@ class Controller_Ajax_Highlitingpoi extends Controller_Ajax_Admin_Base_Highlitin
     public function action_delete() {
         throw new HTTP_Exception_500(SAFE::message('ehttp','invalid_operation'));
     }
-
+    /*
     public function action_index() {
         throw new HTTP_Exception_500(SAFE::message('ehttp','invalid_operation'));
     }
+    */
 
     public function before() {
         parent::before();
@@ -37,6 +41,73 @@ class Controller_Ajax_Highlitingpoi extends Controller_Ajax_Admin_Base_Highlitin
         $_POST['highliting_state_id'] = HSTATE_IN_ACCETTAZIONE;
         // erase front from datastructname
         #$this->_inheritDatastructName = substr($this->_datastructName, 6);
+    }
+
+
+    protected function _default_filter_list($orm)
+    {
+        // get section_highliting_typlogolies
+        $higliting_typologies = ORM::factory('Highliting_Typology')->find_all();
+        $htts = array();
+        foreach ($higliting_typologies as $higliting_typology)
+        {
+            if ($higliting_typology->has('sections', ORM::factory('Section', array('section' => 'FRONTEND'))))
+                $htts[] = $higliting_typology->id;
+        }
+
+        // only typology to show on frontend
+        $orm->join('highliting_typologies','LEFT')
+            ->on($orm->object_name().'.highliting_typology_id','=','highliting_typologies.id')
+            ->where('highliting_typology_id', 'IN', DB::expr('('.implode(',',$htts).')'));
+    }
+    
+    protected function _single_request_row($orm)
+    {
+        $toRes = Controller_Ajax_Base_Crud::_single_request_row($orm);
+
+        $this->_unset_ORMGIS_geofield($toRes);
+        $this->_set_the_geom($toRes, $orm);
+
+        # data to unset:
+        foreach ( array(
+            'highliting_state_id',
+            'highliting_user_id',
+            'protocol_user_id',
+            'supervisor_user_id',
+            'data_ins',
+            'data_mod',
+            'ending',
+            'executor_user_id',
+            'highliting_path_id',
+            'strut_ric',
+            'pt_inter',
+            'aree_attr',
+            'insediam',
+            'pt_acqua',
+            'pt_socc',
+            'percorr',
+            'fatt_degr',
+            'stato_segn',
+            'tipo_segna',
+
+
+
+            ) as $toUnSet)
+        {
+            unset($toRes[$toUnSet]);
+        }
+
+        $images = $orm->images->find_all();
+        $toRes['images'] = array();
+        foreach($images as $image)
+            $toRes['images'] = array(
+                'image_url' => $this->_image_uri.$image->file,
+                'image_thumb_url' => $this->_image_thumb_uri.$image->file,
+                'description' => $image->description
+            );
+
+
+        return $toRes;
     }
 
     protected function _data_edit()
