d5adb628e1e4ff64a0dce85d6496940c97e27387
wlorenzetti <lorenzetti@gis3w.it>
2016-05-22 11:02:23 +0200

    Fix proxy action.

--- proxy.php
+++ proxy.php
@@ -1,7 +1,15 @@
 <?php
 $get = $_GET;
 unset($get['url']);
-$ch = curl_init($_GET['url'].'?'.http_build_query($get));
+
+$url = parse_url($_GET['url'], PHP_URL_SCHEME ).'://'.parse_url($_GET['url'], PHP_URL_HOST ).':'.parse_url($_GET['url'], PHP_URL_PORT ).parse_url($_GET['url'], PHP_URL_PATH );
+$query = [];
+parse_str(parse_url($_GET['url'], PHP_URL_QUERY ), $query);
+foreach ($query as $k => $v)
+{
+    $get[$k] = $v;
+}
+$ch = curl_init($url.'?'.http_build_query($get));
 $output = curl_exec($ch);
 echo $output;
-?>
+
